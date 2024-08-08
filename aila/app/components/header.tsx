f28c29e571a6a71c5d913856c9d30937be3e4cc9
Bora <borabalos9@gmail.com>
2024-08-08 10:30:27 +0200

    Clean code for header links

--- aila/app/components/header.tsx
+++ aila/app/components/header.tsx
@@ -1,15 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
 
+
 const Header = () => {
+
+    const links = [
+        {
+            name: 'About',
+            url: '/about',
+        },
+        {
+            name: 'Contact',
+            url: '/contact',
+        },
+        {
+            name: 'Services',
+            url: '/services',
+        },
+        
+    ];
+
     return (
         <header className="w-full h-16 bg-slate-100 p-4 rounded-2xl flex items-center justify-between">
             <Image src="/aila.svg" width={280} height={280} alt={""} />
 
             <nav className="flex flex-row space-x-8 uppercase">
-                <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
-                <Link href="/services">Services</Link>
+                {links.map((link, index) => (
+                    <Link href={link.url} key={`link${index}`}>
+                        {link.name}
+                    </Link>
+                ))}
             </nav>
         </header>
     );
