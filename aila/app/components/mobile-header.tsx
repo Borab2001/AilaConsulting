a579754d1fc4a01626647c92327b917a47e50c6c
Bora <borabalos9@gmail.com>
2024-09-04 14:59:48 +0200

    Alignment adjustments + layouting for mobile images

--- aila/app/components/mobile-header.tsx
+++ aila/app/components/mobile-header.tsx
@@ -56,27 +56,31 @@ const MobileHeader = () => {
                 </div>
                 
                 <motion.nav 
-                    className="uppercase flex flex-col items-center justify-center space-y-8"
+                    className="flex flex-col items-start justify-center space-y-8"
                     initial={{ height: 0 }}
                     animate={{ height: isActive ? "200px" : 0 }}
                     transition={{ duration: 1, ease: "easeInOut", delay: isActive ? 0 : 0.2 }}
                 >
-                    {links.map((link, index) => (
-                        <TransitionLink 
-                            href={link.url}
-                            key={`link${index}`}
-                            className={`${isActive ? "pointer-events-auto" : "pointer-events-none"} relative after:absolute after:bg-black after:bottom-0 after:left-0 after:h-[1.5px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300`}
-                            onLinkClick={handleLinkClick}
-                        >
-                            <motion.span
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: isActive ? 1 : 0 }}
-                                transition={{ duration: 0.5, ease: "easeInOut", delay: isActive ? 0.7 : 0 }}
-                            >
-                                {link.name}
-                            </motion.span>
-                        </TransitionLink>
-                    ))}
+                    <div className="flex flex-row justify-center items-center">
+                        <div className="uppercase flex flex-col items-start justify-center space-y-8">
+                            {links.map((link, index) => (
+                                <TransitionLink 
+                                    href={link.url}
+                                    key={`link${index}`}
+                                    className={`${isActive ? "pointer-events-auto" : "pointer-events-none"} relative after:absolute after:bg-black after:bottom-0 after:left-0 after:h-[1.5px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300`}
+                                    onLinkClick={handleLinkClick}
+                                >
+                                    <motion.span
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: isActive ? 1 : 0 }}
+                                        transition={{ duration: 0.5, ease: "easeInOut", delay: isActive ? 0.7 : 0 }}
+                                    >
+                                        {link.name}
+                                    </motion.span>
+                                </TransitionLink>
+                            ))}
+                        </div>
+                    </div>
                 </motion.nav>
                 
             </motion.header>
