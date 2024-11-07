2968d212277cc240ad3f000d57e77d9e124ba747
Bora <borabalos9@gmail.com>
2024-11-07 14:50:49 +0100

    Adjusted and extracted globe

---
+++ aila/app/components/ui/globe.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React, { useRef, useEffect } from 'react';
+import createGlobe from 'cobe';
+
+export default function Globe() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            let phi = 0; // Initial rotation angle
+
+            const globe = createGlobe(canvasRef.current, {
+                devicePixelRatio: 2,
+                width: 600, // Adjust the width as needed
+                height: 600, // Adjust the height as needed
+                phi: 0, // Starting rotation
+                theta: 0.3, // Tilt angle
+                dark: 1, // Background set to black
+                diffuse: 1.35, // Diffuse lighting for softer appearance
+                mapSamples: 28000, // Quality of the globe
+                mapBrightness: 8, // Keep brightness neutral for transparency effect
+                // baseColor: [1, 1, 1], // Black globe
+                baseColor: [0.255, 0.255, 0.255], // #414141
+                opacity: 0.85, // Lower opacity
+                markerColor: [0.267, 0, 0.969], // #4400f7
+                glowColor: [0.6, 0.6, 0.6], // #cccccc
+                markers: [
+                    { location: [41.0082, 28.9784], size: 0.08 } // Istanbul
+                ],
+                onRender: (state) => {
+                    state.phi = phi;
+                    phi += 0.005; // Adjust rotation speed here if needed
+                }
+            });
+
+            return () => globe.destroy(); // Cleanup when the component unmounts
+        }
+    }, []);
+
+    return (
+        <canvas ref={canvasRef} />
+    );
+}
--- aila/app/services/page.tsx
+++ aila/app/services/page.tsx
@@ -1,46 +1,9 @@
-"use client";
-
-import React, { useRef, useEffect } from 'react';
-import createGlobe from 'cobe';
+import Globe from "../components/ui/globe";
 
 export default function Services() {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-
-    useEffect(() => {
-        if (canvasRef.current) {
-            let phi = 0; // Initial rotation angle
-
-            const globe = createGlobe(canvasRef.current, {
-                devicePixelRatio: 2,
-                width: 600, // Adjust the width as needed
-                height: 600, // Adjust the height as needed
-                phi: 0, // Starting rotation
-                theta: 0.3, // Tilt angle
-                dark: 1, // Background set to black
-                diffuse: 1.5, // Diffuse lighting for softer appearance
-                mapSamples: 16000, // Quality of the globe
-                mapBrightness: 1, // Keep brightness neutral for transparency effect
-                baseColor: [1, 1, 1], // Black globe
-                opacity: 0.85, // Lower opacity
-                markerColor: [1, 1, 1], // White markers
-                glowColor: [1, 1, 1], // White glow
-                markers: [
-                    { location: [37.7749, -122.4194], size: 0.03 }, // San Francisco
-                    { location: [51.5074, -0.1278], size: 0.03 } // London
-                ],
-                onRender: (state) => {
-                    state.phi = phi;
-                    phi += 0.005; // Adjust rotation speed here if needed
-                }
-            });
-
-            return () => globe.destroy(); // Cleanup when the component unmounts
-        }
-    }, []);
-
     return (
-        <main className="flex flex-1 flex-grow h-full pt-20 justify-center items-center">
-            <canvas ref={canvasRef} />
+        <main className="flex flex-1 flex-grow h-full pt-16 md:pt-0">
+            <Globe />
         </main>
     );
 }
