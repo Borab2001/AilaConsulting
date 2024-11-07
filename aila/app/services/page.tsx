20a26cba0c751956592e1014dd56c6ee7fb711b0
Bora <borabalos9@gmail.com>
2024-11-07 15:46:02 +0100

    Service beams added with a bento in services page

--- aila/app/services/page.tsx
+++ aila/app/services/page.tsx
@@ -5,6 +5,7 @@ import Header from "../components/header";
 import MobileHeader from "../components/mobile-header";
 
 import { motion } from "framer-motion";
+import { ServiceBeams } from "../components/service-beams";
 
 
 export default function Services() {
@@ -33,7 +34,7 @@ export default function Services() {
 				</div>
 
                 {/* Left column */}
-				<div className="col-span-6 row-span-9 grid grid-cols-subgrid grid-rows-subgrid max-lg:col-span-full max-lg:grid-rows-none max-lg:gap-4">
+				<div className="col-span-5 row-span-9 grid grid-cols-subgrid grid-rows-subgrid max-lg:col-span-full max-lg:grid-rows-none max-lg:gap-4">
                     <motion.div 
 						className="p-4 min-h-[288px] col-span-5 row-span-9 max-lg:col-span-8 max-md:col-span-full bg-bento border border-border text-title rounded-2xl w-full flex flex-col justify-between gap-4 relative overflow-hidden"
 						custom={0}
@@ -44,15 +45,26 @@ export default function Services() {
                         <h2 className="text-3xl font-bold mb-4">
 							Transform your <i>international</i> relocation into a breeze with our <i>all-in-one</i> solution.
 						</h2>
-                        <div className="flex-1 w-auto h-auto absolute -right-1/4 -bottom-1/4">
+                        <div className="flex-1 w-auto h-auto flex items-center justify-center absolute -right-1/4 -bottom-1/4">
                             <Globe />
                         </div>
                     </motion.div>
                 </div>
 
                 {/* Right column */}
-                <div className="col-span-6 row-span-9 grid grid-cols-subgrid grid-rows-subgrid max-lg:col-span-full max-lg:grid-rows-none max-lg:gap-4">
-                    
+                <div className="col-span-7 row-span-9 grid grid-cols-subgrid grid-rows-subgrid max-lg:col-span-full max-lg:grid-rows-none max-lg:gap-4">
+                    <motion.div 
+						className="p-4 min-h-[288px] col-span-7 row-span-5 max-lg:col-span-full max-md:col-span-full bg-bento border border-border text-title rounded-2xl w-full flex flex-col justify-between gap-4 relative overflow-hidden"
+						custom={0}
+						initial="hidden"
+						animate="visible"
+						variants={gridVariants}
+					>
+                        <h2 className="text-3xl font-bold mb-4">
+							Translations in multiple languages.
+						</h2>
+                       <ServiceBeams />
+                    </motion.div>
                 </div>
             </div>
         </main>
