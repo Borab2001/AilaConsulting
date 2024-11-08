5a33758d2de462f1878b4a71d891051e9b3889a5
Bora <borabalos9@gmail.com>
2024-11-08 17:01:19 +0100

    Fixing globe disappearing issue

--- aila/app/services/page.tsx
+++ aila/app/services/page.tsx
@@ -14,7 +14,7 @@ export default function Services() {
 			{/* Left column */}
 			<div className="col-span-5 row-span-9 grid grid-cols-subgrid grid-rows-subgrid max-lg:col-span-full max-lg:grid-rows-none max-lg:gap-4">
 				<motion.div 
-					className="p-4 min-h-[288px] col-span-5 row-span-9 max-lg:col-span-8 max-md:col-span-full bg-bento border border-border text-title rounded-2xl w-full flex flex-col justify-between gap-4 relative overflow-hidden"
+					className="p-4 min-h-[700px] lg:min-h-0 col-span-5 row-span-9 max-lg:col-span-8 max-md:col-span-full bg-bento border border-border text-title rounded-2xl w-full flex flex-col justify-between gap-4 relative overflow-hidden"
 					custom={0}
 					initial="hidden"
 					animate="visible"
@@ -23,7 +23,7 @@ export default function Services() {
 					<h2 className="text-3xl font-bold mb-4">
 						Transform your <i>international</i> relocation into a breeze with our <i>all-in-one</i> solution.
 					</h2>
-					<div className="flex-1 w-auto h-auto flex items-center justify-center absolute left-full -right-1/2 -bottom-1/4">
+					<div className="w-[500px] h-[500px] absolute z-10 -right-1/4 -bottom-1/4 flex">
 						<Globe />
 					</div>
 				</motion.div>
