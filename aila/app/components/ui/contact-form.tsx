c17ea3bb2785efee05ef3d98cfbf845eda2faf43
Bora <borabalos9@gmail.com>
2024-11-09 23:46:45 +0100

    Importing components in contact form and installed form component from shadcn

--- aila/app/components/ui/contact-form.tsx
+++ aila/app/components/ui/contact-form.tsx
@@ -1,4 +1,6 @@
-
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const ContactForm = () => {
     return (
