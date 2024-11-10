94db5dc71bfa6f28e1fa7da125d53c01e7594272
Bora <borabalos9@gmail.com>
2024-11-10 13:38:15 +0100

    Done entire contact form ui and installed textarea from shadcn

--- aila/components/contact-form.tsx
+++ aila/components/contact-form.tsx
@@ -9,6 +9,7 @@ import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter } from "next/navigation";
 import { EmailSchema } from "@/schema";
+import { Textarea } from "./ui/textarea";
 
 
 const ContactForm = () => {
@@ -25,10 +26,81 @@ const ContactForm = () => {
         }
     });
 
-    return (
-        <div className="w-full h-full flex flex-col">
+    const onSubmit = async (data: z.infer<typeof EmailSchema>) => {
+        try {
+            console.log(data);
+            toast.success("Your message is sent successfully!");
+            router.refresh();
+        } catch (error) {
+            toast.error("An error occurred. Please try again later.");
+        }
+    }
 
-        </div>
+    return (
+        <Form {...form}>
+            <form
+                className="space-y-6"
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                <FormField
+                    name="firstName"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel htmlFor="firstName">First Name</FormLabel>
+                            <FormControl>
+                                <Input id="firstName" type="text" {...field} />
+                                <FormMessage>{form.formState.errors.firstName?.message}</FormMessage>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    name="lastName"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel htmlFor="lastName">Last Name</FormLabel>
+                            <FormControl>
+                                <Input id="lastName" type="text" {...field} />
+                                <FormMessage>{form.formState.errors.lastName?.message}</FormMessage>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    name="email"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel htmlFor="email">Email</FormLabel>
+                            <FormControl>
+                                <Input id="email" type="email" {...field} />
+                                <FormMessage>{form.formState.errors.email?.message}</FormMessage>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    name="message"
+                    control={form.control}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel htmlFor="message">Message</FormLabel>
+                            <FormControl>
+                                <Textarea id="message" {...field} />
+                                <FormMessage>{form.formState.errors.message?.message}</FormMessage>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+
+                <Button type="submit">Send</Button>
+            </form>
+        </Form>
     );
 }
  
