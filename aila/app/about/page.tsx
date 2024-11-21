2c15014bb97f04e3fa506229a87480ab7ffd7245
Bora <borabalos9@gmail.com>
2024-11-21 17:04:28 +0100

    Dev message in about page

--- aila/app/about/page.tsx
+++ aila/app/about/page.tsx
@@ -3,7 +3,7 @@ import BentoLayout, { gridVariants } from "@/components/bento-layout";
 export default function About() {
     return (
        <BentoLayout gridVariants={gridVariants}>
-            About Page
+            About Page in development...
         </BentoLayout>
     );
 }
