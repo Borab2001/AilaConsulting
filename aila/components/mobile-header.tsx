d19d7829efedfd84c741be52a7ef28bd398d322e
Bora <borabalos9@gmail.com>
2024-11-28 21:35:02 +0100

    Changing size and speeds in mobile header

--- aila/components/mobile-header.tsx
+++ aila/components/mobile-header.tsx
@@ -39,7 +39,7 @@ const MobileHeader = () => {
     };
 
     return (
-        <div className={`inset-0 fixed z-50 h-screen w-full transition-all duration-1000 ${isActive ? "bg-white/20 backdrop-blur-sm" : "bg-transparent backdrop-blur-none pointer-events-none"}`}>
+        <div className={`inset-0 fixed z-50 h-screen w-full transition-all ease-in-out duration-500 ${isActive ? "bg-black/50 backdrop-blur-sm" : "bg-transparent backdrop-blur-none pointer-events-none"}`}>
             <div className="w-full pt-4 px-4 absolute top-0 flex md:hidden items-center bg-gradient-to-b from-background from-50% to-transparent">
                 <motion.header 
                     className={`w-full pointer-events-auto bg-bento border border-border text-title p-4 rounded-2xl flex flex-col transition-all duration-500 ease-in-out`}
@@ -74,18 +74,18 @@ const MobileHeader = () => {
                     
                     <motion.nav 
                         className="flex items-center"
-                        animate={{ height: isActive ? "228px" : "0px" }}
+                        animate={{ height: isActive ? "320px" : "0px" }}
                         initial={{ height: 0 }}
-                        transition={{ duration: 1, ease: "easeInOut", delay: isActive ? 0 : 0.2 }}    
+                        transition={{ duration: 0.5, ease: "easeInOut", delay: isActive ? 0 : 0.2 }}    
                     >
                         <motion.div
                             className="w-full flex flex-row justify-between items-center"
-                            animate={{ height: isActive ? "228px" : "0px", padding: isActive ? "32px 0 8px" : "0" }}
+                            animate={{ height: isActive ? "320px" : "0px", padding: isActive ? "32px 0 0" : "0" }}
                             initial={{ height: 0, padding: 0 }}
-                            transition={{ duration: 1, ease: "easeInOut", delay: isActive ? 0 : 0.2 }}
+                            transition={{ duration: 0.5, ease: "easeInOut", delay: isActive ? 0 : 0.2 }}
                         >
-                            <div className={`flex flex-col items-start justify-center space-y-8 ${isActive ? "w-full" : "w-auto"}`}>
-                                <div className="uppercase flex flex-col items-start justify-center space-y-8">
+                            <div className={`flex flex-col h-full items-center justify-center space-y-8 ${isActive ? "w-full pointer-events-auto" : "w-full pointer-events-none"}`}>
+                                <div className="uppercase flex flex-col flex-1 items-center justify-center space-y-8">
                                     {links.map((link, index) => (
                                         <TransitionLink 
                                             href={link.url}
@@ -98,7 +98,8 @@ const MobileHeader = () => {
                                             <motion.span
                                                 initial={{ opacity: 0 }}
                                                 animate={{ opacity: isActive ? 1 : 0 }}
-                                                transition={{ duration: 0.5, ease: "easeInOut", delay: isActive ? 0.7 : 0 }}
+                                                transition={{ duration: 0.5, ease: "easeInOut", delay: isActive ? 0.2 : 0 }}
+                                                className="text-3xl"
                                             >
                                                 {link.name}
                                             </motion.span>
@@ -108,14 +109,14 @@ const MobileHeader = () => {
                                 <motion.div  
                                     initial={{ opacity: 0 }}
                                     animate={{ opacity: isActive ? 1 : 0 }}
-                                    transition={{ duration: 0.5, ease: "easeInOut", delay: isActive ? 0.7 : 0 }}
+                                    transition={{ duration: 0.5, ease: "easeInOut", delay: isActive ? 0.2 : 0 }}
                                 >
                                     <p className="w-full text-center text-sm text-neutral-400 font-light">
                                         <span className="uppercase text-neutral-600">Copyright: </span>
                                         &#169; Aila Consulting {currentYear}</p>
                                 </motion.div>
                             </div>
-                            <motion.div
+                            {/* <motion.div
                                 className={`relative h-[188px] aspect-square rounded-md overflow-hidden hidden flex-shrink-0 flex-grow-0 pointer-events-none ${isActive ? "sm:block" : "sm:hidden"}`}
                                 initial={{ opacity: 0 }}
                                 animate={{
@@ -130,7 +131,7 @@ const MobileHeader = () => {
                                         alt={links[selectedLink.index].alt}
                                     />
                                 )}
-                            </motion.div>
+                            </motion.div> */}
                         </motion.div>
                     </motion.nav>
                     
