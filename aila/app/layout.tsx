c2ea374c2f6da4b6c11be998907e79fbec2da6f2
Bora <borabalos9@gmail.com>
2024-11-30 21:43:05 +0100

    Correcting metadata structure

--- aila/app/layout.tsx
+++ aila/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -6,15 +7,13 @@ import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: "Aila Consulting",
 	description: "Fast and reliable visa, ikamet, and work permit consulting in Istanbul.",
 	keywords: "visa, ikamet, residence permit, work permit, turkey, istanbul, aila, consulting",
-	charset: "UTF-8",
-	httpEquiv: "X-UA-Compatible",
-	content: "IE=edge",
-	viewport: "width=device-width, initial-scale=1.0",
 };
+
+export const viewport = "width=device-width, initial-scale=1.0";
 
 export default function RootLayout({
 	children,
@@ -24,15 +23,8 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<meta charSet={metadata.charset} />
-				<meta
-                    httpEquiv={metadata.httpEquiv}
-                    content={metadata.content}
-                />
-                <meta name="viewport" content={metadata.viewport} />
-				<title>{metadata.title}</title>
-				<meta name="description" content={metadata.description} />
-                <meta name="keywords" content={metadata.keywords} />
+				<meta charSet="UTF-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 				<meta name="apple-mobile-web-app-title" content="Aila" />
 				<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
 				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
