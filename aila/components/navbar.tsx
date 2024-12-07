10f0e87153d24b002d7476f50191410c17f6aa58
Bora <borabalos9@gmail.com>
2024-12-07 22:55:03 +0100

    Added all navbar data to en json file

--- aila/components/navbar.tsx
+++ aila/components/navbar.tsx
@@ -1,6 +1,8 @@
 import TransitionLink from "@/lib/transition-link";
 import { motion } from "framer-motion";
 
+import { useTranslations } from 'next-intl';
+
 
 export const menuSlide = {
     initial: {
@@ -32,21 +34,23 @@ export const slide = {
 
 const Navbar = () => {
 
+    const t = useTranslations('Navbar');
+
     const links = [
         {
-            name: 'Home',
+            name: t('home'),
             url: '/',
         },
         {
-            name: 'About',
+            name: t('about'),
             url: '/about',
         },
         {
-            name: 'Contact',
+            name: t('contact'),
             url: '/contact',
         },
         {
-            name: 'Services',
+            name: t('services'),
             url: '/services',
         },
         
@@ -66,7 +70,7 @@ const Navbar = () => {
         >
             <div className="h-full p-menu md:py-24 flex flex-col justify-between">
                 <nav className="flex flex-col gap-8 mt-[5vh] md:mt-20 text-5xl text-primary capitalize">
-                    <p className="w-full text-sm uppercase text-subtitle">Navigation</p>
+                    <p className="w-full text-sm uppercase text-subtitle">{t("navigation")}</p>
                     {
                         links.map((link, index) => {
                             return (
@@ -87,7 +91,7 @@ const Navbar = () => {
                     }
                 </nav>
                 <div className="flex flex-col justify-between gap-4 w-full uppercase">
-                    <p className="text-sm uppercase text-subtitle">Copyright</p>
+                    <p className="text-sm uppercase text-subtitle">{t('copyright')}</p>
                     <span className="text-sm text-title capitalize">
                         &#169;{currentYear} Aila Consulting 
                     </span>
