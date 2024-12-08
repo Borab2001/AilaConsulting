52cb4870a4e58aabd2254bdd9502697f625dd0c3
Bora <borabalos9@gmail.com>
2024-12-08 01:47:36 +0100

    Ensure the matcher does not unnecessarily target /en

--- aila/middleware.ts
+++ aila/middleware.ts
@@ -1,11 +1,16 @@
 import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
+import { routing } from './i18n/routing';
  
-export default createMiddleware(routing);
- 
-export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en)/:path*']
-  // matcher: ['/', '/(en|fr|tr)/:path*']
+export default createMiddleware({
+    // Use the existing routing configuration
+    ...routing,
+    defaultLocale: 'en', // Explicitly define 'en' as the default locale
+    localePrefix: 'as-needed', // Remove '/en' prefix from the default locale
+});
 
+export const config = {
+    // Match only internationalized pathnames
+    matcher: ['/', '/:path*']
+    // matcher: ['/', '/(en|fr|tr)/:path*']
+	// matcher: ['/', '/(fr|tr)/:path*']
 };
