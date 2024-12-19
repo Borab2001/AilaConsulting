9f17398420aef1503427b1593635285711785773
Bora <borabalos9@gmail.com>
2024-12-19 05:07:14 +0100

    Cleaned main page code

--- aila/app/[locale]/page.tsx
+++ aila/app/[locale]/page.tsx
@@ -42,10 +42,6 @@ export default function Home() {
 					variants={gridVariants}
 				>
 					<div className="ml-auto">
-						{/* Sparkles icon */}
-						{/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#cccccc" className="size-6">
-							<path fillRule="evenodd" d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z" clipRule="evenodd" />
-						</svg> */}
 
 						{/* Globe icon */}
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#cccccc" className="size-12">
@@ -74,9 +70,6 @@ export default function Home() {
 						</div>
 					</TransitionLink>
 				</motion.div>
-				{/* <div className="relative min-h-[480px] md:min-h-0 col-span-3 row-span-5 max-lg:col-span-4 max-md:col-span-full w-full flex-grow">
-					<Portrait />
-				</div> */}
 
 				{/* Grid Child 4 */}
 				<motion.div 
@@ -87,14 +80,6 @@ export default function Home() {
 					variants={gridVariants}	
 				>
 					<div className="size-[2.75rem]">
-						{/* <Image
-							src="/next.svg"
-							className='w-full h-full'
-							width={48}
-							height={48}
-							alt='flower'
-						/> */}
-						
 						{/* Badge icon */}
 						{/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#999999" className="size-6">
 							<path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
@@ -124,7 +109,6 @@ export default function Home() {
 					animate="visible"
 					variants={gridVariants}
 				>
-					{/* <TransitionLink href="/contact" className="p-4 w-full h-full flex flex-col justify-between bg-primary group hover:bg-neutral-200 text-neutral-900 transition ease-in-out duration-300"> */}
 					<TransitionLink
 						href="/contact"
 						className="relative p-4 w-full h-full flex flex-col justify-between bg-primary group hover:bg-element text-bento hover:text-title transition ease-in-out duration-300"
@@ -161,31 +145,6 @@ export default function Home() {
 					animate="visible"
 					variants={gridVariants}	
 				>
-					{/* <div className="flex flex-col gap-2">
-						<div className="flex flex-row items-center w-full space-x-4">
-							<div className="bg-neutral-300 h-12 w-12 rounded-full flex-shrink-0 flex-grow-0"/>
-							<div className="flex flex-col items-start justify-center">
-								<h2 className="text-2xl font-bold mb-0">Visa Solutions</h2>
-								<span className="">Fast-track your student, work, and family visas.</span>
-							</div>
-						</div>
-
-						<div className="flex flex-row items-center w-full space-x-4">
-							<div className="bg-neutral-300 h-12 w-12 rounded-full flex-shrink-0 flex-grow-0"/>
-							<div className="flex flex-col items-start justify-center">
-								<h2 className="text-2xl font-bold mb-0">Certified Translations</h2>
-								<span className="">Certified translations between French, Turkish, and Russian.</span>
-							</div>
-						</div>
-
-						<div className="flex flex-row items-center w-full space-x-4">
-							<div className="bg-neutral-300 h-12 w-12 rounded-full flex-shrink-0 flex-grow-0"/>
-							<div className="flex flex-col items-start justify-center">
-								<h2 className="text-2xl font-bold mb-0">Residence Permit Assistance</h2>
-								<span className="">Easy applications and renewals with expert support.</span>
-							</div>
-						</div>
-					</div> */}
 					<div className='col-span-4 row-span-8 max-lg:col-span-full h-full flex-grow-0 overflow-y-hidden'>
 						<Work />
 					</div>
@@ -201,7 +160,6 @@ export default function Home() {
 					variants={gridVariants}
 				>
 					{socials.map((social, index) => (
-						// <TransitionLink
 						<Link
 							key={`social${index}`}	
 							href={social.href}
@@ -210,7 +168,6 @@ export default function Home() {
 						>
 							{social.name}
 						</Link>
-						// </TransitionLink>
 					))}
 				</motion.div>
 			</div>
